ed638123c198dd5ec59325ed605ad59ec310593a
Unzila <usshaukat@gmail.com>
2023-08-27 20:51:00 +0500

    first commit

---
+++ Feedback Project/index.js
@@ -0,0 +1,30 @@
+const ratings=document.querySelectorAll(".rating");
+const btn= document.getElementById("btn");
+const container=document.getElementById("container");
+let selectRating="";
+
+ratings.forEach((ratingEle) => {
+    ratingEle.addEventListener("click",(event) => {
+        // console.log(event.target.innerText || event.target.parentNode.innerText);
+        removeActive();
+        selectRating=event.target.innerText || event.target.parentNode.innerText;
+        event.target.classList.add("acive")
+        event.target.parentNode.classList.add("active")
+    });
+});
+
+btn.addEventListener("click", ()=>{
+    if(selectRating!==""){
+        container.innerHTML=`<strong>Thank you</strong>
+        <br><br>
+        <strong>Feedback: ${selectRating}</strong>
+        <p>We'll use your feedback to improve our customer support</p>`
+        
+    }
+})
+
+function removeActive(){
+    ratings.forEach((ratingEle)=>{
+        ratingEle.classList.remove("active");
+    });
+}
